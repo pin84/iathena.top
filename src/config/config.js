8c04013acb4c5b9756d26ec63bd87866051bd8fe
qp1984 <qp1984@live.com>
2018-12-06 12:59:27 +0800

    'deploy'

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const test = 'http://localhost:9000/web/iathena',
-  pro = 'http://data.iathena.top:9000/web/iathena'
+  pro = 'https://data.iathena.top:9000/web/iathena'
 
 export default {
-  url: test
+  url: pro
 }
 
