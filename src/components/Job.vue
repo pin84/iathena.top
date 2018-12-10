d86cdaa1ad812cd8eb2989b8fbdaca98fb6b7850
qp1984 <qp1984@live.com>
2018-12-10 09:49:30 +0800

    ignorecase false

---
+++ src/components/Job.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="wrapper">
+    <div class="title">
+      <span>作品集</span>
+    </div>
+    <ul class="list">
+       <li>
+        <img src="../assets/img/02.jpg" alt="">
+        <a href="http://df.iathena.top" target="_blank">
+          <h4>静态页面-企业官网</h4>
+          <p>业务范围下的数据都是用ajax从后台请求</p>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/01.jpg" alt="">
+        <a href="https://pin84.github.io/live/01_web/01_miaov/" target='_blank'>
+          <h4>静态页面-企业官网</h4>
+          <p></p>
+        </a>
+      </li>
+     
+      <li>
+        <img src="../assets/img/03.jpg" alt="">
+        <a href="https://pin84.github.io/live/05_css3/05_3dPictrues/" target="_blank">
+          <h4>css3 3D旋转</h4>
+          <p>请用 Google Chrome 浏览器打开(原因你懂的)</p>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/04.jpg" alt="">
+        <a href="http://seller.iathena.top" target="_blank">
+          <h4>VUE 移动端，请将浏览器切换到手机端页面</h4>
+          <p>仿'饿了吗'APP，所有数据都用axios从后台请求</p>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/08.jpg" alt="">
+        <a href="http://cxen.iathena.top" target="_blank">
+          <h4>VUE 移动端，请将浏览器切换到手机端页面</h4>
+          <p>前后端分离，新时代英语APP。</p>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/05.jpg" alt="">
+        <a href="https://pin84.github.io/live/01_web/03_ResponsiveWeb/" target="_blank">
+          <h4>理财网站-响应式页面</h4>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/06.jpg" alt="">
+        <a href="https://pin84.github.io/live/02_jspractice/02_magnifier/" target="_blank">
+          <h4>淘宝网的放大镜效果</h4>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/07.jpg" alt="">
+        <a href="https://pin84.github.io/live/02_jspractice/01_JavaScriptDrumKit/" target="_blank">
+          <h4>css+js</h4>
+          <p>来弹钢琴吧~~只支持PC端</p>
+        </a>
+      </li>
+      <li>
+        <img src="../assets/img/08.jpg" alt="">
+        <a href="https://github.com/pin84" target="_blank">
+          <h4>更多请访问我的github</h4>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="stylus" scoped>
+.wrapper
+  width 80%
+  padding-bottom 30px
+  margin 0 auto
+  display flex
+  flex-direction column
+  .title
+    font-size 3rem
+    margin 3rem 0
+    font-weight 600
+    span
+      padding .5rem 0
+      border-bottom 2px solid #E87E04
+  .list
+    display flex
+    flex-wrap wrap
+    justify-content  space-between
+    perspective 750px
+    li
+      width 24%
+      position relative
+      margin 4px
+      transition 300ms
+      transform-origin 0 50%
+      transform-style preserve-3d
+      &:hover
+        transform scale(1.04)
+        a
+          opacity 1
+          
+      img
+        width 100%
+        height 100%
+      a
+        position absolute
+        top 0
+        left 0
+        width 100%
+        height 100%
+        background rgba(0, 0, 0, 0.5)
+        color #fff
+        display flex
+        flex-direction column 
+        align-items center
+        justify-content center
+        font-size 1.4rem
+        opacity 0
+        box-sizing border-box
+        padding 30px
+        line-height 25px
+        h4
+          margin-bottom 10px  
+      // &:hover
+      //   transform scale(1.04)
+      //   a
+      //     opacity 1
+
+
+@media screen and (max-width:900px)
+  .wrapper
+    width 95%
+    .list
+      display flex
+      justify-content start
+      li
+        width 31%
+        margin 4px
+        
+@media screen and (max-width:480px)
+  .wrapper
+    width 95%
+    .list
+      display flex
+      margin-bottom 80px
+      li
+        width 48%
+        margin 2px
+        a
+          opacity 1
+          font-size 1.3rem
+          line-height 18px
+          padding 15px
+          font-weight 100
+          h4
+            font-weight 100
+        &:hover  
+          transform scale(1)    
+</style>
---
+++ src/components/Skill.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="wrapper">
+      <div class="signboard one">
+        <div class="sign">html5、css3、js</div>
+        <div class="strings"></div>
+        <div class="pin top"></div>
+        <div class="pin left"></div>
+        <div class="pin right"></div>
+      </div>
+      <div class="signboard two">
+        <div class="sign">vue、node、mysql</div>
+        <div class="strings"></div>
+        <div class="pin top"></div>
+        <div class="pin left"></div>
+        <div class="pin right"></div>
+      </div>
+      <div class="signboard three">
+        <div class="sign">html5、css3、js、vue、node、mysql</div>
+        <div class="strings"></div>
+        <div class="pin top"></div>
+        <div class="pin left"></div>
+        <div class="pin right"></div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+import Goddess from './goddess.vue'
+export default {
+  components: {
+    Goddess
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper
+  width 100%
+  height 100vh
+  min-height 700px
+  position relative
+  background url(../assets/img/bg3.jpg)   no-repeat center center
+  background-size cover
+  .signboard
+    width 400px
+    height 300px
+    transform-origin 200px 0
+    position absolute
+    &.one
+      top 2%
+      left 10%
+      animation onemove 1.5s ease-in-out infinite alternate
+    &.two
+      top 2%
+      left 60%  
+      animation onemove 1s ease-in-out infinite alternate
+    &.three
+      display none  
+      animation onemove 1.5s ease-in-out infinite alternate
+    .sign
+      width 100%
+      height 200px
+      background burlywood
+      border-radius 15px
+      position absolute
+      bottom 0
+      font-size 40px
+      color saddlebrown 
+      font-weight 600
+      text-align center
+      line-height 200px
+      text-shadow 0 2px 0 rgba(255,255,255,0.3) ,0 -2px 0 rgba(0, 0, 0, 0.7);
+    .strings
+      width 150px
+      height 150px
+      border 5px solid brown 
+      position absolute 
+      transform rotate(45deg)
+      top 38px
+      left 125px
+      border-right none
+      border-bottom none
+    .pin
+      width 25px
+      height 25px
+      border-radius 50%
+      position absolute 
+      &.top
+        background gray 
+        left 187px
+      &.left, &.right
+        background brown 
+        top 110px
+        box-shadow 0 2px 0 rgba(255, 255, 255, 0.3)
+      &.left
+        left 80px
+      &.right 
+        right 80px
+@keyframes onemove{
+  from{
+    transform:rotate(10deg)
+  }
+  to{
+    transform:rotate(-10deg)
+  }
+}
+
+@keyframes twomove{
+  from{
+    transform:rotate(10deg)
+  }
+  to{
+    transform:rotate(-10deg)
+  }
+}
+
+@media screen and (max-width:900px)
+  .wrapper
+    width 100%
+    .signboard
+      width 250px
+      height 190px
+      transform-origin 125px 0
+      &.one
+        top 12%
+        left 10%
+      &.two
+        top 12%
+        left 60%
+      .sign
+        height 130px
+        font-size 25px
+        line-height 120px
+      .strings
+        width 100px
+        height 100px
+        top 20px
+        left 75px
+      .pin
+        position absolute 
+        &.top
+          left 115px
+          top -4px
+        &.left, &.right
+          top 65px
+        &.left
+          left 44px
+        &.right 
+          right 40px  
+@media screen and (max-width:768px) 
+  .wrapper
+    width 100%
+    .signboard
+      width 200px
+      height 360px
+      transform-origin 100px 0
+      top 80px
+      left 10vw
+      position relative
+      &.one , &.two
+        display none
+      &.three
+        display block  
+      .sign
+        height 310px
+        font-size 30px
+        line-height 50px
+        box-sizing border-box
+        padding 50px 0 0 8px
+      .strings
+        width 80px
+        height 80px
+        top 20px
+        left 60px
+      .pin
+        position absolute 
+        &.top
+          left 44%
+          top -4px
+        &.left, &.right
+          top 50px
+        &.left
+          left 30px
+        &.right 
+          right 30px           
+</style>
