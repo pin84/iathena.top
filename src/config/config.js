1e43ac2b15bb1359d344716c155bc783907e34e4
pin84 <qp1984@live.com>
2019-03-13 16:07:37 +0800

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const test = 'http://localhost:9000/web/iathena',
   pro = 'https://data.iathena.top/web/iathena',
-  html8 = 'http://data.html8.info/web/iathena',
+  html8 = 'https://data.html8.info/web/iathena',
   html8Avartor = 'https://html8.info/'
 
 export default {
