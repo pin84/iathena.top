4d9f51c160987a800cb96f11095fd168615c0c88
pin84 <qp1984@live.com>
2019-03-17 22:45:35 +0800

    test

--- src/components/works/Candle.vue
+++ src/components/works/Candle.vue
@@ -23,33 +23,36 @@ export default {
   width 260px
   height 260px
   border-radius 5px
-  background-color #fff
+  background-color black
   box-shadow 0 2px 30px rgba(0,0,0,0.2)
   &:hover
     box-shadow 0 2px 30px #FF0033
   #candle
     position relative
+    display flex
+    justify-content center
     width 100px
-    height 200px
-    font-size 10px
-    // border 1px solid red
-    // background-color darkorange
+    height 140px
+    bottom -120px
+    left 50%
+    transform translateX(-50%)
+    // font-size 10px
     background linear-gradient(
       orange,
       darkorange,
       sienna,
-      saddlebrown 50%,
+      saddlebrown 70%,
       rgba(0,0,0,0.6)
     )
     border-radius 100px / 40px
     box-shadow:
-      inset 2em -3em 5em rgba(0, 0, 0, 0.4),
-      inset -2em 0 5em rgba(0, 0, 0, 0.4);
+      inset 20px -30px 50px rgba(0, 0, 0, 0.4),
+      inset -20px 0 50px rgba(0, 0, 0, 0.4);
     &::before
       content ''
       position absolute 
       width inherit
-      height 50px
+      height 30px
       border 2px solid darkorange
       border-radius 50%
       box-sizing border-box
@@ -61,6 +64,73 @@ export default {
         darkorange
       )
       filter opacity(0.7)
-
+    .thread
+      position absolute 
+      width 6px
+      height 36px
+      top -18px
+      background linear-gradient(
+        #111,
+        black,
+        orange 90%
+      )
+      border-radius 40% 40% 0 0
+    .flames
+      position absolute 
+      width 18px
+      height 60px
+      &::before //内焰
+        content ''
+        position absolute 
+        width inherit 
+        height 60px
+        background royalblue
+        top -48px
+        border-radius 50% 50% 35% 35%
+        border 2px solid dodgerblue
+        box-sizing border-box
+        filter opacity(0.7)
+      &::after
+        content ''  
+        position absolute 
+        width inherit 
+        height 80px
+        top -80px
+        background linear-gradient(white 80%,transparent)
+        border-radius 50% 50% 20% 20%
+        box-shadow 0 -6px 4px darkorange
+        animation move 5s linear infinite
+    .glow
+      position absolute 
+      width 50px
+      height 120px
+      background-color orangered 
+      border-radius 50%
+      top -105px
+      filter blur(40px)
+      // animation blink 500ms infinite
+@keyframes move {
+  0%,100%{
+    transform:rotate(2deg)
+  }
+  50%{
+    transform:rotate(-2deg)
+  }
+}
+@keyframes enlarge {
+  0%,100% {
+    height: 120px
+    top -120px
+  }
+  50%{
+    height:140px
+  }
+}
+@keyframes blink {
+    to {
+        filter: blur(6em) opacity(0.8);
+    }
+}
+    
 
 </style>
